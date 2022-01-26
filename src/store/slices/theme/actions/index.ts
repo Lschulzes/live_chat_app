9f96e9bb153592322dc51147cfa5b3f184ac8061
lschulzes <lucassilvaiee@gmail.com>
2022-01-26 19:55:44 -0300

    Refactor persistOrGetlocalStorage

--- src/store/slices/theme/actions/index.ts
+++ src/store/slices/theme/actions/index.ts
@@ -1,22 +1,22 @@
-import { DefaultTheme } from 'styled-components';
-import { ThemeStateType } from '..';
-import { darkTheme, lightTheme } from '../../../../styles/themes/Themes';
-import { persistOrGetLocalstorage } from '../../../helpers';
+import { DefaultTheme } from "styled-components";
+import { ThemeStateType } from "..";
+import { darkTheme, lightTheme } from "../../../../styles/themes/Themes";
+import { persistLocalstorage, getLocalstorage } from "../../../helpers";
 
 function getDifferentTheme(themeTitle: string): DefaultTheme {
-  const newTheme = themeTitle === 'light' ? darkTheme : lightTheme;
-  persistOrGetLocalstorage('theme', newTheme, true);
+  const newTheme = themeTitle === "light" ? darkTheme : lightTheme;
+  persistLocalstorage("theme", newTheme);
   return newTheme;
 }
 
 export function toggleThemeAction(state: ThemeStateType) {
-  const theme = persistOrGetLocalstorage<DefaultTheme>('theme', state.theme);
+  const theme = getLocalstorage<DefaultTheme>("theme", state.theme);
   state.theme = getDifferentTheme(theme.title);
   return state;
 }
 
 export function loadThemeAction(state: ThemeStateType) {
-  const theme = persistOrGetLocalstorage<DefaultTheme>('theme', state.theme);
+  const theme = getLocalstorage<DefaultTheme>("theme", state.theme);
   state.theme = theme;
   return state;
 }
